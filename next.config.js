7ecde41779dcd46ca4c227208716d6a3c85da03d
dinhquyle <dinhquyle@gmail.com>
2023-01-04 16:22:44 +0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images : {
+    domains : ["placeimg.com","media.geeksforgeeks.org","localhost"],
+  }
+}
+
+module.exports = nextConfig
